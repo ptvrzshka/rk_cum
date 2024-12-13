7d88d3c31293b5a431c6dacdf035a150ec382286
DESKTOP-SBRO3IF <mikhail@ubuntu>
2024-12-13 14:58:42 +0300

    wip

--- Tests/main/main.cpp
+++ Tests/main/main.cpp
@@ -55,52 +55,52 @@ void ProcessLoop()
 	}
 }
 
-
+// Глобальная структура данных
 typedef struct
 {
-  gboolean white;
+  GstBuffer *buffer;
   GstClockTime timestamp;
 } MyContext;
 
-/* called when we need to give data to appsrc */
-static void
-need_data (GstElement * appsrc, guint unused, MyContext * ctx)
-{
-	std::unique_lock<std::mutex> lock(procMtx);
-	procCond.wait(lock);
+// Callback для предоставления новых данных
+static void need_data(GstElement * appsrc, guint unused, MyContext * ctx) {
 
-	GstBuffer *buffer;
-  	guint size;
-  	GstFlowReturn ret;
+	// std::unique_lock<std::mutex> lock(procMtx);
+	// procCond.wait(lock);
 
-  	size = WIDTH * HEIGHT * 2;
+	guint size;
+	GstFlowReturn ret;
 
-  	buffer = gst_buffer_new_allocate (NULL, size, NULL);
+	size = WIDTH * HEIGHT * 2;
 
-  /* this makes the image black/white */
-  	gst_buffer_memcmp (buffer, 0, frame, size);
-	//gst_buffer_memset (buffer, 0, ctx->white ? 0xff : 0x00, size);
+    ctx->buffer = gst_buffer_new_allocate(NULL, WIDTH * HEIGHT * 2, NULL);
+    GstMapInfo map;
+    gst_buffer_map(ctx->buffer, &map, GST_MAP_WRITE);
 
-  	ctx->white = !ctx->white;
+    // Копируем данные в буфер
+    memcpy(map.data, frame, WIDTH * HEIGHT * 2);
+    gst_buffer_unmap(ctx->buffer, &map);
 
-  /* increment the timestamp every 1/2 second */
-	GST_BUFFER_PTS (buffer) = ctx->timestamp;
-	GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 50);
-	ctx->timestamp += GST_BUFFER_DURATION (buffer);
+	// ctx->white = !ctx->white;
 
-	g_signal_emit_by_name (appsrc, "push-buffer", buffer, &ret);
-	gst_buffer_unref (buffer);
+	/* increment the timestamp every 1/2 second */
+	GST_BUFFER_PTS (ctx->buffer) = ctx->timestamp;
+	GST_BUFFER_DURATION (ctx->buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 50);
+	ctx->timestamp += GST_BUFFER_DURATION (ctx->buffer);
+
+	g_signal_emit_by_name (appsrc, "push-buffer", ctx->buffer, &ret);
+	gst_buffer_unref (ctx->buffer);
 
 	// cv::Mat image_cv(HEIGHT, WIDTH, CV_16UC1, frame);
-	// imshow("Original", image_cv);
-	// cv::waitKey(1);
+	// imshow("Cam", image_cv);
+
 	framesCnt += 1;
 	if (framesCnt == 30)
 		CalcFps();
 }
 
-
-static void media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media,
+static void
+media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media,
     gpointer user_data)
 {
 	GstElement *element, *appsrc;
@@ -117,13 +117,13 @@ static void media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media
 	/* configure the caps of the video */
 	g_object_set (G_OBJECT (appsrc), "caps",
 		gst_caps_new_simple ("video/x-raw",
-			"format", G_TYPE_STRING, "GRAY16_BE",
+			"format", G_TYPE_STRING, "GRAY16_LE",
 			"width", G_TYPE_INT, WIDTH,
 			"height", G_TYPE_INT, HEIGHT,
 			"framerate", GST_TYPE_FRACTION, 50, 1, NULL), NULL);
 
 	ctx = g_new0 (MyContext, 1);
-	ctx->white = FALSE;
+	ctx->buffer = nullptr;
 	ctx->timestamp = 0;
 	/* make sure ther datais freed when the media is gone */
 	g_object_set_data_full (G_OBJECT (media), "my-extra-data", ctx,
@@ -135,151 +135,55 @@ static void media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media
 	gst_object_unref (element);
 }
 
-int main(int argc, char *argv[]) 
-{
-	InitializeSocket(new int[4] { 192, 168, 0, 1 }, 50016);
-	InitializeCamera(new int[4] { 192, 168, 0, 15 }, 50000);
+int main(int argc, char *argv[]) {
 
-    std::thread readerThread(&ReaderLoop);
+	InitializeSocket(new int[4] { 192, 168, 0, 1}, 50016);
+	InitializeCamera(new int[4] { 192, 168, 0, 15}, 50000);
 
-	  // Initialize GStreamer
-    gst_init(&argc, &argv);
+	std::thread readerThread(&ReaderLoop);
 
-    // Create an RTSP server
-    GstRTSPServer *server = gst_rtsp_server_new();
-    gst_rtsp_server_set_address(server, "127.0.0.1"); // Set the server IP
+	GMainLoop *loop;
+	GstRTSPServer *server;
+	GstRTSPMountPoints *mounts;
+	GstRTSPMediaFactory *factory;
 
-    // Create a mount point for the RTSP stream
-    GstRTSPMountPoints *mounts = gst_rtsp_server_get_mount_points(server);
-    GstRTSPMediaFactory *factory = gst_rtsp_media_factory_new();
+	gst_init (&argc, &argv);
 
-    // Configure the media factory with a pipeline to loop a video
-    gst_rtsp_media_factory_set_launch(factory,
-        "( appsrc name=mysrc ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96  )");
+	loop = g_main_loop_new (NULL, FALSE);
 
-	g_signal_connect (factory, "media-configure", (GCallback) media_configure, NULL);
+	/* create a server instance */
+	server = gst_rtsp_server_new ();
+    // gst_rtsp_server_set_address(server, "192.168.88.41"); // Set the server IP
 
-    // Add the mount point
-    gst_rtsp_mount_points_add_factory(mounts, "/test", factory);
-    g_object_unref(mounts);
+	/* get the mount points for this server, every server has a default object
+	* that be used to map uri mount points to media factories */
+	mounts = gst_rtsp_server_get_mount_points (server);
 
-    // Start the RTSP server
-    gst_rtsp_server_attach(server, NULL);
-    g_print("Stream ready at rtsp://127.0.0.1:8554/test\n");
+	/* make a media factory for a test stream. The default media factory can use
+	* gst-launch syntax to create pipelines.
+	* any launch line works as long as it contains elements named pay%d. Each
+	* element with pay%d names will be a stream */
+	factory = gst_rtsp_media_factory_new ();
+	gst_rtsp_media_factory_set_launch (factory,
+		"( appsrc name=mysrc ! videoconvert ! video/x-raw,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency key-int-max=25 ! rtph264pay name=pay0 pt=96 )");
 
-    // Run the main loop
-    g_main_loop_run(g_main_loop_new(NULL, FALSE));
-    return 0;
+	/* notify when our media is ready, This is called whenever someone asks for
+	* the media and a new pipeline with our appsrc is created */
+	g_signal_connect (factory, "media-configure", (GCallback) media_configure,
+		NULL);
 
-	// GMainLoop *loop;
-	// GstRTSPServer *server;
-	// GstRTSPMountPoints *mounts;
-	// GstRTSPMediaFactory *factory;
+	/* attach the test factory to the /test url */
+	gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
 
-	// gst_init (&argc, &argv);
+	/* don't need the ref to the mounts anymore */
+	g_object_unref (mounts);
 
-	// loop = g_main_loop_new (NULL, FALSE);
+	/* attach the server to the default maincontext */
+	gst_rtsp_server_attach (server, NULL);
 
-	// /* create a server instance */
-	// server = gst_rtsp_server_new ();
-
-	// /* get the mount points for this server, every server has a default object
-	// * that be used to map uri mount points to media factories */
-	// mounts = gst_rtsp_server_get_mount_points (server);
-
-	// /* make a media factory for a test stream. The default media factory can use
-	// * gst-launch syntax to create pipelines.
-	// * any launch line works as long as it contains elements named pay%d. Each
-	// * element with pay%d names will be a stream */
-	// factory = gst_rtsp_media_factory_new ();
-	// gst_rtsp_media_factory_set_launch (factory,
-	// 	"( appsrc name=mysrc ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 )");
-
-	// /* notify when our media is ready, This is called whenever someone asks for
-	// * the media and a new pipeline with our appsrc is created */
-	// g_signal_connect (factory, "media-configure", (GCallback) media_configure,
-	// 	NULL);
-
-	// /* attach the test factory to the /test url */
-	// gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
-
-	// /* don't need the ref to the mounts anymore */
-	// g_object_unref (mounts);
-
-	// /* attach the server to the default maincontext */
-	// gst_rtsp_server_attach (server, NULL);
-
-	// /* start serving */
-	// g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
-	// g_main_loop_run (loop);
+	/* start serving */
+	g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
+	g_main_loop_run (loop);
 
 	return 0;
-
-	// // std::thread processorThread(&ProcessLoop);
-
-	// // init_image_processor(frameOut);
-
-    // std::cout << "Start reading" << std::endl;
-
-	// int key = 0;
-	// int fs_cnt = 0;
-	// startTime = std::chrono::system_clock::now();
-	// while (true)
-	// {
-	// 	// std::unique_lock<std::mutex> lock(visMtx);
-	// 	// visCond.wait(lock);
-
-	// 	std::unique_lock<std::mutex> lock(procMtx);
-	// 	procCond.wait(lock);
-
-	// 	if (fs_cnt != 0) 
-	// 	{
-	// 		if (fs_cnt == 1)
-	// 			calc_fs(frame, fs_cnt, true);
-	// 		calc_fs(frame, fs_cnt, false);
-	// 		fs_cnt += 1;
-	// 		if (fs_cnt > 16)
-	// 			fs_cnt = 0;
-	// 	}
-
-	// 	framesCnt += 1;
-	// 	if (framesCnt == 30)
-	// 		CalcFps();
-
-	// 	// cv::Mat image_cv(HEIGHT, WIDTH, CV_16UC1, frame);
-	// 	// cv::Mat image_cv_out(HEIGHT, WIDTH, CV_16UC1, frameOut);
-
-	// 	// imshow("Original", image_cv);
-	// 	// imshow("Processed", image_cv_out);
-
-	// 	// key = cv::waitKey(1);
-	// 	// if (key == 99) {
-	// 	// 	int packet_data[6] = { 0x05, 0x5C, 0xCF, 0x00, 0x00, 0x00 };
-	// 	// 	SendData(packet_data, 6); // Команда на калибровку 
-	// 	// }
-	// 	// if (key == 98) {
-	// 	// 	int packet_data[6] = { 0x05, 0x5C, 0x00, 0x00, 0x0E, 0x80 };
-	// 	// 	SendData(packet_data, 6); // Команда на Прямой проход 
-	// 	// }
-	// 	// if (key == 97) {
-	// 	// 	SendFrequence(25);
-	// 	// 	SendIntTime(25000);
-	// 	// }
-	// 	// if (key == 100) {
-	// 	// 	fs_cnt = 1;
-	// 	// }
-	// 	// else if (key == 27) break;
-	// }
-
-	// stop_flag = true;
-
-	// std::cout << "Wait..." << std::endl;
-
-	// readerThread.join();
-	// CloseSocket();
-	// delete[] frame;
-
-	// std::cout << "Complited" << std::endl;
-
-    // return 0;
 }
